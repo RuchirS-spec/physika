ecc5e3f1e462a1715e85c5bd1cbdeed8a7704891
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-07-23 00:54:17 +0530

    added more examples in import statement physika file

--- examples/torch_code/example_import_statement.py
+++ examples/torch_code/example_import_statement.py
@@ -3,9 +3,17 @@ import torch.nn as nn
 import torch.optim as optim
 from physika.runtime import DEVICE
 
+<<<<<<< HEAD
 from physika.runtime import print
+=======
+from physika.runtime import physika_print
+from physika.runtime import compute_grad
+>>>>>>> 1963e7f (added more examples in import statement physika file)
 
 # === Functions ===
+def f_x(x):
+    return (x ** 2)
+
 def fact(n):
     if n == 0.0:
         return 1.0
@@ -34,6 +42,26 @@ def superbee(r):
     phi = (0.5 * (s3 + torch.abs(s3 if isinstance(s3, torch.Tensor) else torch.tensor(float(s3)))))
     return phi
 
+# === Classes ===
+class ExampleClass(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        self.learnable_params = []
+
+    def class_method(self):
+        this = self
+        return 1
+
+    @property
+    def params(self):
+        return list(self.parameters())
+
+    def update(self, lr, grads):
+        with torch.no_grad():
+            for p, g in zip(self.parameters(), grads):
+                if g is not None:
+                    p -= lr * g
+
 # === Program ===
 x = 1.0
 fact_results = fact(x)
@@ -41,4 +69,17 @@ print(fact_results)
 torch_funcs_results = torch_funcs_with_scalar_R(x)
 print(torch_funcs_results)
 f_results = f(x)
-print(f_results)
+physika_print(f_results)
+obj_example_class = ExampleClass().to(DEVICE)
+class_value = obj_example_class.class_method()
+physika_print(class_value)
+v = torch.tensor([1.0, 2.0, 3.0], device=DEVICE)
+for i in range(int(0), int(3)):
+    if v[int(i)] > 2:
+        v[int(i)] = (v[int(i)] * 2)
+physika_print(v)
+grad_f_x = compute_grad(f_x, v[int(2)])
+physika_print(grad_f_x)
+r = torch.tensor([(-1.0), 0.0, 0.5, 1.0, 2.0], device=DEVICE)
+φ = superbee(r)
+physika_print(φ)
