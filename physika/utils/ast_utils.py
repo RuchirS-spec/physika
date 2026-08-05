3fb5fd29b262e06292b3966a4257a0e4fbbc6d63
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-08-06 01:11:31 +0530

    updated the ast utils to handle physika_print inside emit_body_stmts

--- physika/utils/ast_utils.py
+++ physika/utils/ast_utils.py
@@ -1083,6 +1083,9 @@ def emit_body_stmts(
             expr_code = generate_solve_call(expr)
             lines.append(f"{prefix}{var_name} = {expr_code}")
             known_vars.append(var_name)
+        elif stmt_op == "expr":
+            _, expr, _ = stmt
+            lines.append(f"{prefix}{ast_to_torch_expr(expr)}")
         elif stmt_op == "body_assign":
             _, var_name, expr = stmt
             expr_code = generate_solve_call(expr)
