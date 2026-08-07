82e5dcdb818e6560e02efbc8892104733bf5d653
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-08-07 20:19:22 +0530

    updated the test_runtime to use print() function

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -50,20 +50,20 @@ class TestComplMul1d:
 
 
 class TestPhysikaPrint:
-    """Tests for ``physika_print()`` function"""
+    """Tests for ``print()`` function"""
 
     def test_physika_print_program_level(self):
-        # Test ``physika_print()`` at program level.
+        # Test ``print()`` at program level.
         src = ("x: ℝ = 1\n"
-               "physika_print(x)\n")
+               "print(x)\n")
         assert type_errors(src) == []
         out = capture_output(src)
         assert "1" in out
 
     def test_physika_print_function_level(self):
-        # Test ``physika_print()`` in function level.
+        # Test ``print()`` in function level.
         src = ("def test_f(x: ℝ): ℝ:\n"
-               "    physika_print(x)\n"
+               "    print(x)\n"
                "    return 1\n"
                "test_f(10)\n")
         assert type_errors(src) == []
@@ -71,10 +71,10 @@ class TestPhysikaPrint:
         assert "10" in out
 
     def test_physika_print_inside_class(self):
-        # Test ``physika_print()`` inside class.
+        # Test ``print()`` inside class.
         src = ("class Temp:\n"
                "    def f(x: ℝ): ℝ:\n"
-               "        physika_print(x)\n"
+               "        print(x)\n"
                "        return 1\n"
                "obj: Temp = Temp()\n"
                "obj.f(10)\n")
@@ -83,20 +83,20 @@ class TestPhysikaPrint:
         assert "10" in out
 
     def test_physika_print_inside_loop(self):
-        # Test ``physika_print()`` inside loops.
+        # Test ``print()`` inside loops.
         src = ("arr: ℝ[2] = [1, 2]\n"
                "for i: ℕ(2):\n"
-               "    physika_print(arr[i])\n")
+               "    print(arr[i])\n")
         assert type_errors(src) == []
         out = capture_output(src)
         assert "1" in out
         assert "2" in out
 
     def test_physika_print_inside_function_loop(self):
-        # Test ``physika_print()`` inside function loops.
+        # Test ``print()`` inside function loops.
         src = ("def test_f(x: ℝ[m]): ℝ:\n"
                "    for k:ℕ(2):\n"
-               "        physika_print(x[k])\n"
+               "        print(x[k])\n"
                "    return 1\n"
                "arr: ℝ[2] = [10, 20]\n"
                "test_f(arr)")
